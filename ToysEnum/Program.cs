7690f6f26386136836585b1fa3dc1092bed587cc
Alex Ku <alex.ku96@outlook.com>
2019-02-07 15:52:35 -0500

    Add project files.

---
+++ ToysEnum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToysEnumLibrary;
+
+namespace ToysEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildrenToys toys = new ChildrenToys(1);
+
+            toys.ToysCalculation();
+        }
+    }
+}
---
+++ ToysEnumLibrary/ChildrenToys.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToysEnumLibrary
+{
+    public struct ChildrenToys
+    {
+        enum Children { ChildOne, ChildTwo, ChildThree }
+        private int[] Marbles;
+        private int[] Spinners;
+        private int[] Legos;
+
+        public ChildrenToys(int x)
+        {
+            Marbles = new int[(int)Children.ChildThree + x];
+            Spinners = new int[(int)Children.ChildThree + x];
+            Legos = new int[(int)Children.ChildThree + x];
+        }
+
+        public void ToysCalculation()
+        {
+            Children child = Children.ChildOne;
+
+            foreach (Children children in (Children[])Enum.GetValues(typeof(Children)))
+            {
+                Console.WriteLine($"--{child}--");
+                Console.Write("Marbles: ");
+                Marbles[(int)child] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Spinners: ");
+                Spinners[(int)child] = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Legos: ");
+                Legos[(int)child] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                child++;
+
+
+            }
+            ChildMarbles();
+            ChildSpinners();
+            ChildLegos();
+            Console.ReadLine();
+        }
+
+        public void ChildMarbles()
+        {
+            Children children = Children.ChildOne;
+
+            Console.WriteLine("----Marbles----");
+
+            foreach (int child in Marbles)
+            {
+                Console.WriteLine($"{children}: {child.ToString()}");
+                children++;
+            }
+        }
+        public void ChildSpinners()
+        {
+            Children children = Children.ChildOne;
+
+            Console.WriteLine("----Spinners----");
+
+            foreach (int child in Spinners)
+            {
+                Console.WriteLine($"{children}: {child.ToString()}");
+                children++;
+            }
+        }
+        public void ChildLegos()
+        {
+            Children children = Children.ChildOne;
+
+            Console.WriteLine("----Legos----");
+
+            foreach (int child in Legos)
+            {
+                Console.WriteLine($"{children}: {child.ToString()}");
+                children++;
+            }
+        }
+
+
+    }
+}
